ec67823cddad0b89e1ead56de5f2b1874a17e651
AntonRevutskij <anton.revutskij@webpp.ru>
2023-10-03 14:41:22 +0300

    Add test

--- tests/plugins/identity/conftest.py
+++ tests/plugins/identity/conftest.py
@@ -11,7 +11,7 @@ from server.apps.identity.models import User
 
 
 @final
-class UserData(TypedDict, total=False):
+class UserDataForm(TypedDict, total=False):
     """
     Represent the simplified user data that is required to create a new user.
     Importing this type is only allowed under ``if TYPE_CHECKING`` in tests.
@@ -26,11 +26,12 @@ class UserData(TypedDict, total=False):
     lead_id: bool
     is_staff: bool
     is_active: bool
-
+    password1: str
+    password2: str
 
 @final
 class UserDataFactory(Protocol):
-    def __call__(self, **fields: Unpack[UserData], ) -> UserData:
+    def __call__(self, **fields: Unpack[UserDataForm], ) -> UserDataForm:
         """User data factory protocol."""
 
 
@@ -38,32 +39,31 @@ class UserDataFactory(Protocol):
 def user_data_factory() -> UserDataFactory:
     """Returns factory for fake random data for regitration."""
 
-    def factory(**fields: Unpack[UserData]) -> UserData:
+    def factory(**fields: Unpack[UserDataForm]) -> UserDataForm:
         mf = Field(locale=Locale.RU)
+        password = mf('password')
         schema = Schema(schema=lambda: {
             'email': mf('person.email'),
             'first_name': mf('person.first_name'),
             'last_name': mf('person.last_name'),
-            'date_of_birth': mf('datetime.date'),
+            'date_of_birth': '2000-11-11',
             'address': mf('address.city'),
             'job_title': mf('person.occupation'),
             'phone': mf('person.telephone'),
-            # 'lead_id': mf('increment'),
-            # 'is_staff': False,
-            # 'is_active': True,
         }, iterations=1)
         return {**schema.create()[0],  # type: ignore[misc]
+                **{'password1': password, 'password2': password},
                 **fields}
 
     return factory
 
 
-UserAssertion: TypeAlias = Callable[[str, UserData], None]
+UserAssertion: TypeAlias = Callable[[str, UserDataForm], None]
 
 
 @pytest.fixture(scope='session')
 def assert_correct_user() -> UserAssertion:
-    def factory(email: str, expected: UserData) -> None:
+    def factory(email: str, expected: UserDataForm) -> None:
         user = User.objects.get(email=email)
 
         assert user.id
--- tests/plugins/identity/test_user_create.py
+++ tests/plugins/identity/test_user_create.py
@@ -1,17 +1,49 @@
+import json
 from http import HTTPStatus
+from pprint import pprint
 
 import pytest
 from django.test import Client
 from django.urls import reverse
+import logging
+
+from server.apps.identity.infrastructure.django.forms import RegistrationForm
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+
+
+def del_key_in_data(data: dict, key: str) -> 'UserData':
+    """
+    We need to simplify registration data to drop passwords.
+    Basically, it is the same as ``registration_data``, but without passwords.
+    """
+    return {  # type: ignore[return-value]
+        key_name: value_part
+        for key_name, value_part in data.items()
+        if not key_name.startswith(key)
+    }
 
 
 @pytest.mark.django_db()
-def test_create_user(client: Client, user_data_factory: 'UserDataFactory', ):
+def test_create_valid_user(client: Client, user_data_factory: 'UserDataFactory', ):
     post_data = user_data_factory()
-    response = client.post(
-        reverse('identity:registration'),
-        data=post_data
-    )
 
+    registration_form = RegistrationForm(post_data)
+    assert registration_form.is_valid() == True
+
+    response = client.post("/identity/registration", data=registration_form.data)
+    assert response.status_code == HTTPStatus.FOUND
+
+
+@pytest.mark.django_db()
+@pytest.mark.parametrize('field', ("email", 'first_name', 'last_name',
+                                   'address', 'job_title', 'phone'))
+def test_no_valid_user_form(client: Client, user_data_factory: 'UserDataFactory', field):
+    post_data = del_key_in_data(user_data_factory(), field)
+
+    registration_form = RegistrationForm(post_data)
+    assert registration_form.is_valid() == False
+
+    response = client.post("/identity/registration", data=registration_form.data)
     assert response.status_code == HTTPStatus.OK
-
